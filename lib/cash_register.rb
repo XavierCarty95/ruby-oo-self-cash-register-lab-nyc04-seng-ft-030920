d28fef5a3532bbbc2fb997cf0ef2a41239c43cbc
Xavier Carty <xaviercarty@gmail.com>
2020-03-12 21:45:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+class CashRegister 
+  
+attr_accessor :total ,:discount
+def initialize(discount=0)
+ @total = 0
+ @discount = discount
+ 
+end
 
+def add_item(title,price,quantity=0)
+
+  if quantity > 0 
+    self.total = price * quantity
+  else 
+    self.total += price
+     @total = self.total
+  end
+    
+ end
+  
+end 
